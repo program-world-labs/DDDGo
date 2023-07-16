1d70e80f40d098ccdef8582238b14a4a151555b7
技術處-平台部-架構組James_Wang <james_wang@mail.chungyo.net>
2023-07-17 01:00:23 +0800

    fix: 修正unit test 錯誤問題

--- pkg/operations/tracer.go
+++ pkg/operations/tracer.go
@@ -1,5 +1,9 @@
 package operations
 
+import (
+	"errors"
+)
+
 // InitNewTracer -.
 func InitNewTracer(host string, _ int, batcher string, sampleRate float64, enabled bool) error {
 	if !enabled {
@@ -9,6 +13,10 @@ func InitNewTracer(host string, _ int, batcher string, sampleRate float64, enabl
 	switch batcher {
 	case "gcp":
 		GoogleCloudOperationInit(host, sampleRate)
+	case "jaeger":
+		return errors.New("jaeger is not supported yet")
+	default:
+		return errors.New("batcher is not supported")
 	}
 
 	return nil
